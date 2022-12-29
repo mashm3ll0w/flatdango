285671a7c1bbc48e15f111a59fc6f0fe7f248b02
Charles Swaleh <charlswall@live.com>
2022-12-29 12:18:30 +0300

    Completed the function to render a menu of all the film names

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const baseURL = "http://localhost:3000/films"
 
 document.addEventListener("DOMContentLoaded", () => {
   loadFilm()
+  showFilms()
 })
 
 function loadFilm(id = 1){
@@ -23,4 +24,24 @@ function populateData(film){
   document.getElementById("film-info").textContent = film.description
   document.getElementById("showtime").textContent = film.showtime
   document.getElementById("ticket-num").textContent = parseInt(film.capacity - film.tickets_sold)
+}
+
+function showFilms(){
+  fetch(baseURL)
+  .then(res => res.json())
+  .then(films => renderMenu(films))
+  .catch(error => console.log("Error: ", error.message))
+}
+
+function renderMenu(films){
+  const ul = document.getElementById("films")
+  ul.innerHTML = ""
+  
+  films.map(film => {
+    const li = document.createElement("li")
+    li.textContent = film.title
+    li.dataset.id = film.id
+    li.classList.add("film")
+    ul.appendChild(li)
+  })
 }
