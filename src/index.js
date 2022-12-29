7645e2a7be7e8033a959af1a9079770f7642bfa9
Charles Swaleh <charlswall@live.com>
2022-12-29 13:45:35 +0300

    Completed the function to buy tickets and update when a movie is sold out

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const baseURL = "http://localhost:3000/films"
 document.addEventListener("DOMContentLoaded", () => {
   loadFilm()
   showFilms()
+  document.getElementById("buy-ticket").addEventListener("click", buyTicket)
 })
 
 function loadFilm(id = 1){
@@ -23,7 +24,12 @@ function populateData(film){
   document.getElementById("runtime").textContent = `${film.runtime} minutes`
   document.getElementById("film-info").textContent = film.description
   document.getElementById("showtime").textContent = film.showtime
+  document.getElementById("ticket-num").dataset.capacity = film.capacity
+  document.getElementById("ticket-num").dataset.sold = film.tickets_sold
   document.getElementById("ticket-num").textContent = parseInt(film.capacity - film.tickets_sold)
+  if (film.capacity - film.tickets_sold === 0){
+    document.querySelector(`#films [data-id="${film.id}"]`).classList.add("sold-out")
+  }
 }
 
 function showFilms(){
@@ -44,4 +50,19 @@ function renderMenu(films){
     li.classList.add("film")
     ul.appendChild(li)
   })
+}
+
+function buyTicket(){
+  let ticketsContainer = document.getElementById("ticket-num")
+  const capacity = parseInt(ticketsContainer.dataset.capacity)
+  let ticketsSold = parseInt(ticketsContainer.dataset.sold)
+  ticketsContainer.textContent -= 1
+  ticketsSold += 1
+  document.getElementById("ticket-num").dataset.sold = ticketsSold
+
+  if(capacity - ticketsSold === 0){
+    document.getElementById("buy-ticket").disabled = true
+    document.getElementById("buy-ticket").textContent = "Sold Out"
+    document.querySelector(`#films [data-id="${document.getElementById("title").dataset.id}"]`).classList.add("sold-out")
+  }
 }
